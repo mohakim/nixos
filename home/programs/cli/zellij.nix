2ea81972b195bd4a700b406deb63fdf81acd80c3
Mohamed Abdihakim <m.abdihakim@proton.me>
2025-07-02 11:06:18 +0800

    updated zellij

--- home/programs/cli/zellij.nix
+++ home/programs/cli/zellij.nix
@@ -1,4 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
+
+let
+  # Simple shell wrapper that checks for .envrc in current directory
+  direnvShell = pkgs.writeShellScript "direnv-shell" ''
+    if [[ -f .envrc ]]; then
+      ${pkgs.direnv}/bin/direnv exec . ${pkgs.fish}/bin/fish
+    else
+      exec ${pkgs.fish}/bin/fish
+    fi
+  '';
+in
 {
   programs.zellij.enable = true;
 
@@ -9,6 +20,9 @@
     copy_on_select false
     scroll_buffer_size 10000
     default_mode "normal"
+    session_serialization false   
+    // Use direnv-aware shell by default
+    default_shell "${direnvShell}"
     
     keybinds {
       normal {
@@ -21,6 +35,16 @@
         // Tab navigation (your existing scheme)
         bind "Super u" { GoToPreviousTab; }
         bind "Super o" { GoToNextTab; }
+
+        // Avoid keyd conflicts
+        unbind "Alt h"
+        unbind "Alt j" 
+        unbind "Alt k"
+        unbind "Alt l"
+        unbind "Alt Left"
+        unbind "Alt Right" 
+        unbind "Alt Up"
+        unbind "Alt Down"
       }
     }
   '';
